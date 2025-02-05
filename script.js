a88e0c7df42c32369e2898d9941a4cedf41a6115
zesh18 <129366212+zesh18@users.noreply.github.com>
2025-02-05 11:42:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function appendValue(value){
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay(){
+  document.getElementById("display").value = "";
+}
+
+function calculate(){
+  const display = document.getElementById("display")
+  try{
+      display.value = eval(display.value)
+  }catch{
+      display.value = "error"
+  }
+}
